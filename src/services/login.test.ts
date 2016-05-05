6f2cffeac76167d55381b71e2d33e91f534ad80e
Brendan Moore <brendan.moore@rangle.io>
2016-05-05 15:17:57 -0400

    Clean up unecessarsry console loging, reduce number of cient generated server auth requiest

--- src/services/login.test.ts
+++ src/services/login.test.ts
@@ -20,69 +20,31 @@ import {
   beforeEach,
   beforeEachProviders
 } from '@angular/core/testing';
-// import {
-//   ComponentFixture,
-//   TestComponentBuilder
-// } from '@angular/compiler/testing';
-// import { By } from '@angular/platform-browser';
-// import { Injectable, Component, provide } from '@angular/core';
 
 describe('LoginService tests', () => {
-  // let builder;
 
   beforeEachProviders(() => {
     return [
       LoginService
     ];
   });
-
-  // beforeEach(inject([TestComponentBuilder], (tcb) => {
-  //   builder = tcb;
-  // }));
   
-  it('should be null', () => {
-    console.log('am i here?');
-    expect( null ).toBeNull();
-  });
-
-  it('should be 4', () => {
-    console.log('am i here?');
-    expect( 2*2 ).toBe(4);
-  });
 
   it('should return a promise', () => {
-    console.log('test 10');
     const loginService = new LoginService();
-    console.log('test 11');
     let loginPromise = loginService.login('user', 'pass');
-    console.log('test 12');
     let payload = {promise: loginPromise};
-    console.log('test 13');
     expect(isPromise(payload)).toBe(true);
-    console.log('test 14');
   });
-
-  it('should show that async is working', 
-    async(inject([  ], () => {
-      let somePromise = new Promise<string>((resolve, reject) => { 
-        resolve('a string'); 
-      });
-      somePromise
-      .then((data) => {
-        expect( null ).toBeNull();
-      });
-  })));
 
   it('should login successfully',
     async(inject([ LoginService ], (loginService: LoginService) => {
-      //let loginService = new LoginService();
       loginService.login('user', 'pass')
       .then((data)=>{
         //success
         expect(data).not.toBeUndefined();
         
       });
-
   })));
 
   it('should try login unsuccessfully (invalid user)',
@@ -106,5 +68,4 @@ describe('LoginService tests', () => {
       expect(err).not.toBeUndefined();
     });
   })));
-
 });
