7f972062fef47b8d0fa3660b37bcf61bb957210e
troy.tao <troy.tao@rangle.io>
2016-09-02 16:35:39 -0400

    Update zone.js to 0.6.17

--- src/tests.entry.ts
+++ src/tests.entry.ts
@@ -6,6 +6,7 @@ import 'zone.js/dist/zone';
 import 'zone.js/dist/sync-test';
 import 'zone.js/dist/async-test';
 import 'zone.js/dist/fake-async-test';
+import 'zone.js/dist/proxy';
 import 'zone.js/dist/jasmine-patch';
 
 import 'ts-helpers';
@@ -36,3 +37,5 @@ testContext.keys().forEach(
       testContext(key);
     }
   });
+
+// testContext('./components/alert/alert.component.test.ts');
