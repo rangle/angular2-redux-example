d9b952a82f3accabf728e4ff06477794fab7055b
Seth Davenport <seth@rangle.io>
2017-02-19 21:38:19 -0500

    some tweaks

--- src/epics/session.epics.ts
+++ src/epics/session.epics.ts
@@ -16,15 +16,18 @@ import { Action } from 'redux';
 import 'rxjs/add/operator/do';
 
 // A fake API on the internets: see http://httpbin.org.
+// Credentials are 'user', 'pass' :)
 const AUTH_URL = 'https://httpbin.org/basic-auth/user/pass';
-const FAKE_USER_DATA = {
-  id: 'user',
-  token: 'abcd1234',
+const USER_DATA_URL = 'http://www.mocky.io/v2/58aa539210000034064b6214';
+
+interface IUserData {
+  id: string;
+  token: string;
   profile: {
-    firstName: 'John',
-    lastName: 'Doe',
-  }
-};
+    firstNam: string;
+    lastName: string;
+  };
+}
 
 @Injectable()
 export class SessionEpics {
@@ -33,25 +36,30 @@ export class SessionEpics {
   login = (action$: Observable<IPayloadAction>) => {
     return action$
       .filter<IPayloadAction>(({ type }) => type === SessionActions.LOGIN_USER)
-      .mergeMap<IPayloadAction, IPayloadAction>(({ payload }) => {
-        return this.doAuthRequest(payload.username, payload.password)
-          .map<Response, IPayloadAction>(result => ({
+      .mergeMap<IPayloadAction, IPayloadAction>(({ payload }) =>
+        this.doAuthRequest(payload.username, payload.password)
+          .map<IUserData, Action>(userData => ({
             type: SessionActions.LOGIN_USER_SUCCESS,
-            payload: FAKE_USER_DATA,
+            payload: userData,
           }))
-          .catch<any, Action>(() => Observable.of({
-            type: SessionActions.LOGIN_USER_ERROR
-          }));
-        });
+          .catch<any, IPayloadAction>(error => Observable.of({
+            type: SessionActions.LOGIN_USER_ERROR,
+            error,
+          })));
   }
 
-  doAuthRequest(username, password) {
+  doAuthRequest(username, password): Observable<IUserData> {
+    // Note that this is intended as an example of how to do HTTP with a
+    // redux-observable epic. It's NOT INTENDED AS A GOOD EXAMPLE OF SECURING
+    // A SITE WITH AUTHENTICATION! For one thing, the user data isn't protected.
     const options = new RequestOptions({
       headers: new Headers({
         Authorization: 'Basic ' + btoa(`${username}:${password}`),
       }),
     });
 
-    return this.http.get(AUTH_URL, options);
+    return this.http.get(AUTH_URL, options)
+      .mergeMap(response => this.http.get(USER_DATA_URL))
+      .map(response => response.json());
   }
 }
